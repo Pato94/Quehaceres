09bbe1e0d17636f05edd151d8694f2808f2b3602
Pato <Pato9406@gmail.com>
2018-10-29 20:07:18 -0300

    Add camera permission handling

--- app/src/main/java/dadm/frba/utn/edu/ar/quehaceres/activities/GroupActivity.kt
+++ app/src/main/java/dadm/frba/utn/edu/ar/quehaceres/activities/GroupActivity.kt
@@ -1,8 +1,10 @@
 package dadm.frba.utn.edu.ar.quehaceres.activities
 
+import android.Manifest
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
 
@@ -10,6 +12,8 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.view.Menu
 import android.view.MenuItem
@@ -24,85 +28,123 @@ import kotlinx.android.synthetic.main.activity_group.*
 
 class GroupActivity : AppCompatActivity(), AvailableTasksFragment.Listener, MyTasksFragment.Listener {
 
-    override fun onMyTaskClicked(item: DummyContent.DummyItem?) {
-        AlertDialog.Builder(this)
-            .setMessage(item!!.content)
-            .setPositiveButton("Verificar") { _, _ -> Toast.makeText(this, "Verificar clicked", Toast.LENGTH_SHORT).show() }
-            .setNegativeButton("Cancelar") { d, _ -> d.dismiss() }
-            .show()
+  override fun onMyTaskClicked(item: DummyContent.DummyItem?) {
+    item?.let {
+      AlertDialog.Builder(this)
+          .setTitle(it.id)
+          .setMessage(it.content)
+          .setPositiveButton("Verificar") { _, _ -> onVerifyClicked() }
+          .setNegativeButton("Cancelar") { d, _ -> d.dismiss() }
+          .show()
     }
+  }
 
-    override fun onAvailableTaskClicked(item: DummyContent.DummyItem?) {
-        AlertDialog.Builder(this)
-            .setMessage(item!!.content)
-            .setPositiveButton("Asignar") { _, _ -> Toast.makeText(this, "Asignar clicked", Toast.LENGTH_SHORT).show() }
-            .setNegativeButton("Cancelar") { d, _ -> d.dismiss() }
-            .show()
+  override fun onAvailableTaskClicked(item: DummyContent.DummyItem?) {
+    item?.let {
+      AlertDialog.Builder(this)
+          .setTitle(it.id)
+          .setMessage(it.content)
+          .setPositiveButton("Asignar") { _, _ -> Toast.makeText(this, "Asignar clicked", Toast.LENGTH_SHORT).show() }
+          .setNegativeButton("Cancelar") { d, _ -> d.dismiss() }
+          .show()
     }
+  }
 
-    companion object {
-        fun newIntent(group: Group, context: Context): Intent {
-            val intent = Intent(context, GroupActivity::class.java)
-            intent.putExtra("GROUP", group)
-            return intent
+  companion object {
+    const val CAMERA_REQUEST_ID = 1001
+
+    fun newIntent(group: Group, context: Context): Intent {
+      val intent = Intent(context, GroupActivity::class.java)
+      intent.putExtra("GROUP", group)
+      return intent
+    }
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_group)
+
+    setUpViews()
+  }
+
+  private fun setUpViews() {
+    val group = intent.getParcelableExtra<Group>("GROUP")
+    setSupportActionBar(toolbar)
+    supportActionBar?.title = group.name
+
+    setUpAdapter()
+  }
+
+  private fun setUpAdapter() {
+    container.adapter = SectionsPagerAdapter(supportFragmentManager)
+
+    container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+    tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
+  }
+
+  private fun onVerifyClicked() {
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+      if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+        // TODO
+      } else {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST_ID)
+      }
+    } else {
+      startVerification()
+    }
+  }
+
+  private fun startVerification() {
+    Toast.makeText(this, "Tenemos permiso", Toast.LENGTH_SHORT).show()
+  }
+
+  override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    when (requestCode) {
+      CAMERA_REQUEST_ID -> {
+        // If request is cancelled, the result arrays are empty.
+        if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+          startVerification()
+        } else {
+          Toast.makeText(this, "No tenemos permiso", Toast.LENGTH_SHORT).show()
         }
+        return
+      }
+      else -> { }
+    }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    menuInflater.inflate(R.menu.menu_group, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    val id = item.itemId
+
+    if (id == R.id.action_notifications) {
+      val intent = NotificationsActivity.newIntent(this)
+      startActivity(intent)
+      return true
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_group)
+    return super.onOptionsItemSelected(item)
+  }
 
-        setUpViews()
+  inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+      return when (position) {
+        0 -> AvailableTasksFragment.newInstance(1)
+        else -> MyTasksFragment.newInstance(1)
+      }
     }
 
-    private fun setUpViews() {
-        val group = intent.getParcelableExtra<Group>("GROUP")
-        setSupportActionBar(toolbar)
-        supportActionBar?.title = group.name
-
-        setUpAdapter()
+    override fun getCount(): Int {
+      return 2
     }
-
-    private fun setUpAdapter() {
-        container.adapter = SectionsPagerAdapter(supportFragmentManager)
-
-        container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
-        tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_group, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-        if (id == R.id.action_notifications) {
-            val intent = NotificationsActivity.newIntent(this)
-            startActivity(intent)
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
-
-    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-
-        override fun getItem(position: Int): Fragment {
-            return when (position) {
-                0 -> AvailableTasksFragment.newInstance(1)
-                else -> MyTasksFragment.newInstance(1)
-            }
-        }
-
-        override fun getCount(): Int {
-            return 2
-        }
-    }
+  }
 }
